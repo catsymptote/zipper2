b13b5ab59542774ce084b5b6aa1a62f3f1c5857b
catsymptote <catsymptote@gmail.com>
2018-03-22 23:51:58 +0100

    Initial comments.

---
+++ compress.py
@@ -0,0 +1,9 @@
+
+# Get files
+# Exclude non-zip files
+# Get password
+# Zip files
+    # Delete originals where successful
+
+
+# (if input is a file, compress or uncompress only this file)
---
+++ decompress.py
@@ -0,0 +1,9 @@
+
+# Get files
+# Exclude non-zip files
+# Get password
+# unzip files
+    # Delete zip archieves where successful
+
+
+# (if input is a file, compress or uncompress only this file)
